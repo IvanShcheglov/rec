89fde2016709e3955b1a15796b990e289333a533
IvanShcheglov <sheglov.ivan@mail.ru>
2024-04-04 18:35:33 +0300

    1 задание

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+int Prompt(string message)
+{
+  Console.Write(message);
+  int result = Convert.ToInt32(Console.ReadLine());
+  return result;
+}
+
+int NaturalNumber(int n, int m)
+{
+  if (n == m) return n;
+  else Console.Write($"{NaturalNumber(n, m - 1)}, ");
+  return m;
+}
+
+int n = Prompt("N: ");
+if (n < 1)
+{
+  Console.WriteLine("Введите положительное число");
+  return;
+}
+
+int m = Prompt("M: ");
+if (m < 1)
+{
+  Console.WriteLine("Введите положительное число");
+  return;
+}
+if (n>m)
+Console.WriteLine("Введите N>M");
+else Console.WriteLine(NaturalNumber(n, m));
